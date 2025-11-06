c39be60863057ebd9cb82e0e7e33b506123aca62
AngelBlack15 <antoniobent.2005@gmail.com>
2025-11-05 23:59:54 -0600

    feat: Actualización de la interfaz a diseño de tarjetas y corrección de conflictos

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   server: {
     proxy: {
       '/api': {
@@ -14,6 +13,4 @@ export default defineConfig({
       }
     }
   }
-=======
->>>>>>> 94aa22234555d93fb52fdb5cf0da388cc203cc7d
 })
